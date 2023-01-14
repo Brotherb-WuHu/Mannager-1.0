d37c87655089511a056b56193f3df81e108d8476
brother_b <2816207652@qq.com>
2023-01-14 23:24:44 +0800

    feat: 文档对比

---
+++ src/views/main/analysis/overview/cpns/board/board.vue
@@ -0,0 +1,47 @@
+<template>
+  <el-card shadow="always" class="box-card">
+    <template #header>
+      <div class="card-header">
+        <span>错误提示</span>
+      </div>
+    </template>
+    <div class="text item">
+      <span>修改提示</span>
+    </div>
+  </el-card>
+</template>
+
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  setup() {
+    return {}
+  }
+})
+</script>
+
+<style scoped lang="less">
+.card-header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+
+.text {
+  font-size: 14px;
+}
+
+.item {
+  margin-bottom: 10px;
+}
+
+.box-card {
+  width: 90%;
+  border-left: 4px solid rgb(233, 176, 32);
+}
+.box-card:hover {
+  // background-color: rgb(206, 82, 44);
+}
+</style>
---
+++ src/views/main/analysis/overview/cpns/side-bar/side-bar.vue
@@ -0,0 +1,72 @@
+<template>
+  <div class="side-bar">
+    <el-button type="primary" class="subBtn">开始校对</el-button>
+
+    <el-card shadow="always" class="box-card">
+      <template #header>
+        <div class="card-header">
+          <span>所有提示</span>
+        </div>
+      </template>
+      <div class="text item">
+        <span>高概率:</span>
+      </div>
+      <div class="text item">
+        <span>低概率:</span>
+      </div>
+      <div class="text item">
+        <span>敏感词:</span>
+      </div>
+    </el-card>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  setup() {
+    return {}
+  }
+})
+</script>
+
+<style scoped lang='less'>
+.side-bar {
+  margin-top: 200px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+
+.card-header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+
+.text {
+  font-size: 14px;
+}
+
+.subBtn {
+  width: 120px;
+  height: 60px;
+  text-align: center;
+  border-radius: 8px;
+  font-size: 16px;
+  padding: 0;
+  color: #fff;
+  background: #0a56d6;
+}
+
+.item {
+  margin-bottom: 10px;
+}
+
+.box-card {
+  width: 120px;
+  margin-top: 15px;
+}
+</style>
